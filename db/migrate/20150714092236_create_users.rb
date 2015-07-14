7f7e462123230e2c8f9bcb4bd5252a8ea5809c4b
haonh <nguyen.hoang.hao@framgia.com>
2015-07-14 17:04:54 +0700

    Init-model

---
+++ db/migrate/20150714092236_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :avatar
+      t.boolean :admin
+      t.string :remember_digest
+
+      t.timestamps null: false
+    end
+  end
+end
