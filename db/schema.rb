402e9f125d1fdfd78a970b487ffccce95257cfcf
haonh <nguyen.hoang.hao@framgia.com>
2015-07-15 14:52:22 +0700

    Signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714095919) do
+ActiveRecord::Schema.define(version: 20150715065356) do
 
   create_table "activities", force: :cascade do |t|
     t.datetime "time"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20150714095919) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "words", force: :cascade do |t|
     t.string   "content"
