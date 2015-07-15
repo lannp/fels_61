d2175154e78366a604ca55cdf15e8328204c190d
lannp <nguyen.phuong.lan@framgia.com>
2015-07-15 13:28:36 +0700

    Fix-Static-Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
-	def full_title(page_title = '')
-    base_title = "E-learning System"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
+  def full_title page_title = ""
+    base_title = t "static_pages.title"
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
