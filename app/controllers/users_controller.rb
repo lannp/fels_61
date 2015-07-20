824d3e8b3928cdc1c79546ae9a341c6a8184ae6e
lannp <nguyen.phuong.lan@framgia.com>
2015-07-20 14:53:49 +0700

    user-avatar-update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find params[:id]
   end
 
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find params[:id]
   end
 
   def create
@@ -21,8 +29,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "edit_user.profile_update"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit :name, :email, :password, :password_confirmation
+    params.require(:user).permit :name, :email, :password, :password_confirmation, :avatar
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "edit_user.log_in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless current_user? @user
   end
 end
